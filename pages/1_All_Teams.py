5ac86578b189feb7700140c317c5e91764e59deb
Pavithra <ravipatipavithra3612@gmail.com>
2026-03-17 12:11:22 -0600

    Create 1_All_Teams.py

---
+++ pages/1_All_Teams.py
@@ -0,0 +1,68 @@
+import streamlit as st
+
+st.title("All Teams Dashboard")
+st.write("Use the links below to open team projects.")
+
+st.subheader("Team 1")
+st.page_link("team1/DashboardFrontPage.py", label="Main Page")
+st.page_link("team1/1_Custom Histogram.py", label="Histogram")
+st.page_link("team1/2_Powerful Data Parser.py", label="Data Parser")
+st.page_link("team1/3_Data Visualizer.py", label="Data Visualizer")
+st.page_link("team1/4_Credits & Attributions.py", label="Credits")
+
+st.subheader("Team 3")
+st.page_link("team3/app.py", label="Open Team 3")
+
+st.subheader("Team 5")
+st.page_link("team5/streamlit_app.py", label="Open Team 5")
+
+st.subheader("Team 6")
+st.page_link("teram6/streamlit.py", label="Open Team 6")
+
+st.subheader("Team 7")
+st.page_link("team7/app.py", label="Open Team 7")
+
+st.subheader("Team 8")
+st.page_link("team8/streamlit_app.py", label="Open Team 8")
+
+st.subheader("Team 9")
+st.page_link("team9/streamlit_app.py", label="Open Team 9")
+
+st.subheader("Team 10")
+st.page_link("team10/streamlit_app.py", label="Open Team 10")
+
+st.subheader("Team 11")
+st.page_link("team11/app.py", label="Open Team 11")
+
+st.subheader("Team 12")
+st.page_link("team12/app.py", label="Open Team 12")
+
+st.subheader("Team 13")
+st.page_link("team13/App.py", label="Main Page")
+st.page_link("team13/California_Housing_(Streamlit).py", label="Visualization 1")
+st.page_link("team13/Streamlit_Test.py", label="Visualization 2")
+
+st.subheader("Team 14")
+st.page_link("team14/app.py", label="Open Team 14")
+
+st.subheader("Team 15")
+st.page_link("team15/app.py", label="Open Team 15")
+
+st.subheader("Team 16")
+st.page_link("team16/alejandro_water_data_analysis.py", label="Open Team 16")
+
+st.subheader("Team 17")
+st.page_link("team17/app.py", label="Open Team 17")
+
+st.subheader("Team 18")
+st.page_link("team18/streamlit_app.py", label="Main Page")
+st.page_link("team18/StockCrash.py", label="Visualization")
+
+st.subheader("Team 19")
+st.page_link("team19/app.py", label="Open Team 19")
+
+st.subheader("Team 20")
+st.page_link("team20/streamlit_app.py", label="Main Page")
+st.page_link("team20/1_What_Goes_Into_EJI.py", label="Visualization 1")
+st.page_link("team20/2_EJI_Scale_and_Categories.py", label="Visualization 2")
+st.page_link("team20/3_change_over_years_and_comparison.py", label="Visualization 3")
