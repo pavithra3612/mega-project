b26117ab04c946c35d203a9aaa6757ce84c6cb49
Pavithra <ravipatipavithra3612@gmail.com>
2026-03-17 12:39:33 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 from st_pages import add_page_title, get_nav_from_toml
 
-use_sections = st.sidebar.toggle("Group by Sections", value=True, key="use_sections_toggle_main")
-
 nav = get_nav_from_toml(".streamlit/pages_sections.toml")
 
 pg = st.navigation(nav)
