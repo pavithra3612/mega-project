237f907c2d34ddba09ad28664da1665c32434b5a
Pavithra <ravipatipavithra3612@gmail.com>
2026-03-17 11:56:04 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,10 @@
 import streamlit as st
-from st_pages import show_pages_from_config
 
 st.set_page_config(
     page_title="ENG220 Mega Project Dashboard",
     page_icon="🏠",
     layout="wide"
 )
-
-show_pages_from_config()
 
 st.title("ENG220 Mega Project Dashboard")
 st.write("Welcome to the combined dashboard for all team projects.")
