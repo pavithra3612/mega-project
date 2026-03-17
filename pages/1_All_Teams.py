213c2733604afdb5416398ac81d2eb47ea1dfcbb
Pavithra <ravipatipavithra3612@gmail.com>
2026-03-17 12:27:45 -0600

    Update 1_All_Teams.py

--- pages/1_All_Teams.py
+++ pages/1_All_Teams.py
@@ -1,43 +1,118 @@
-import streamlit as st
+Team 1
+Folder: team1
 
-st.title("All Teams Dashboard")
-st.write("This page lists all team projects included in the repository.")
+Files:
 
-teams = [
-    ("Team 1", "team1", [
-        "DashboardFrontPage.py",
-        "1_Custom Histogram.py",
-        "2_Powerful Data Parser.py",
-        "3_Data Visualizer.py",
-        "4_Credits & Attributions.py",
-    ]),
-    ("Team 3", "team3", ["app.py"]),
-    ("Team 5", "team5", ["streamlit_app.py"]),
-    ("Team 6", "teram6", ["streamlit.py"]),
-    ("Team 7", "team7", ["app.py"]),
-    ("Team 8", "team8", ["streamlit_app.py"]),
-    ("Team 9", "team9", ["streamlit_app.py"]),
-    ("Team 10", "team10", ["streamlit_app.py"]),
-    ("Team 11", "team11", ["app.py"]),
-    ("Team 12", "team12", ["app.py"]),
-    ("Team 13", "team13", ["App.py", "California_Housing_(Streamlit).py", "Streamlit_Test.py"]),
-    ("Team 14", "team14", ["app.py"]),
-    ("Team 15", "team15", ["app.py"]),
-    ("Team 16", "team16", ["alejandro_water_data_analysis.py"]),
-    ("Team 17", "team17", ["app.py"]),
-    ("Team 18", "team18", ["streamlit_app.py", "StockCrash.py"]),
-    ("Team 19", "team19", ["app.py"]),
-    ("Team 20", "team20", [
-        "streamlit_app.py",
-        "1_What_Goes_Into_EJI.py",
-        "2_EJI_Scale_and_Categories.py",
-        "3_change_over_years_and_comparison.py",
-    ]),
-]
+team1/DashboardFrontPage.py
+team1/1_Custom Histogram.py
+team1/2_Powerful Data Parser.py
+team1/3_Data Visualizer.py
+team1/4_Credits & Attributions.py
+Team 3
+Folder: team3
 
-for team_name, folder, files in teams:
-    st.subheader(team_name)
-    st.write(f"Folder: `{folder}`")
-    st.write("Files:")
-    for f in files:
-        st.code(f"{folder}/{f}")
+Files:
+
+team3/app.py
+Team 5
+Folder: team5
+
+Files:
+
+team5/streamlit_app.py
+Team 6
+Folder: teram6
+
+Files:
+
+teram6/streamlit.py
+Team 7
+Folder: team7
+
+Files:
+
+team7/app.py
+Team 8
+Folder: team8
+
+Files:
+
+team8/streamlit_app.py
+Team 9
+Folder: team9
+
+Files:
+
+team9/streamlit_app.py
+Team 10
+Folder: team10
+
+Files:
+
+team10/streamlit_app.py
+Team 11
+Folder: team11
+
+Files:
+
+team11/app.py
+Team 12
+Folder: team12
+
+Files:
+
+team12/app.py
+Team 13
+Folder: team13
+
+Files:
+
+team13/App.py
+team13/California_Housing_(Streamlit).py
+team13/Streamlit_Test.py
+Team 14
+Folder: team14
+
+Files:
+
+team14/app.py
+Team 15
+Folder: team15
+
+Files:
+
+team15/app.py
+Team 16
+Folder: team16
+
+Files:
+
+team16/alejandro_water_data_analysis.py
+Team 17
+Folder: team17
+
+Files:
+
+team17/app.py
+Team 18
+Folder: team18
+
+Files:
+
+team18/streamlit_app.py
+team18/StockCrash.py
+Team 19
+Folder: team19
+
+Files:
+
+team19/app.py
+Team 20
+Folder: team20
+
+Files:
+
+team20/streamlit_app.py
+team20/1_What_Goes_Into_EJI.py
+team20/2_EJI_Scale_and_Categories.py
+team20/3_change_over_years_and_comparison.py
